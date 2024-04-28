c4ba1061aee609c34e6c06ded1ab692affbf6289
Ricardo Alvarez <rasoftdev@outlook.com>
2024-04-27 22:37:00 -0500

    [FIXED] Fixed redirect routes

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,9 +1,10 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+/*import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';*/
+window.location = '/admin';
 </script>
 
-<template>
+<!--<template>
     <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -19,4 +20,4 @@ import Welcome from '@/Components/Welcome.vue';
             </div>
         </div>
     </AppLayout>
-</template>
+</template>-->
